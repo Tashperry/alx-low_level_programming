c7756f1fbe4d1a8a08701cc9eb098c872062310e
Oluwatimileyin Aseperi <timmieash@gmail.com>
2022-09-08 09:09:39 -0700

    hello puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <studio.h>
+
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n);
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
