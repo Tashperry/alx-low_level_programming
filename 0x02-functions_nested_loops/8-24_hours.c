2a0ec7c6eec9235056f4dbf1a6d9108d1b9c1df0
Oluwatimileyin Aseperi <timmieash@gmail.com>
2022-09-14 05:04:28 -0700

    hours 24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - function that prints every minute of the day of 
+ * jack_bauer - Prints the minutes of a day
  * Return: no return
  */
 void jack_bauer(void)
