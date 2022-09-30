fdb8f5ab63ca9c78cb7a721c5f30b2323e42a105
Oluwatimileyin Aseperi <timmieash@gmail.com>
2022-09-30 16:51:20 -0700

    argcargv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print argument count
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0 or 1
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
