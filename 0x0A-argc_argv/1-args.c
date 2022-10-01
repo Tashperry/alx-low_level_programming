573c1ea41ee13bb4dca653d5cbf15a7352c1c8c3
Oluwatimileyin Aseperi <timmieash@gmail.com>
2022-09-30 17:11:52 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print argument count
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
